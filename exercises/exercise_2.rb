d9a8cdb502803816f3bd09d131e84f299813be72
lenilsamuel <lenilvarghese@gmail.com>
2022-04-05 19:18:45 -0600

    Update store1 name to Edmonton. Finish Exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find_by(id:1)
+puts "Before update --> #{@store1.name}"
+@store2 = Store.find_by(id: 2)
+
+@store1.name = 'Edmonton'
+@store1.save
+
+puts "After update --> #{@store1.name}"
