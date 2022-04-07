9c782e044af94047b45577d4879dafd81d01f38c
lenilsamuel <lenilvarghese@gmail.com>
2022-04-06 21:20:18 -0600

    Use sum, average and method chaining to finish Exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+total_sales = Store.sum(:annual_revenue)
+puts "Total sales is $#{total_sales}"
+
+avg_sales = Store.average(:annual_revenue)
+puts "Average sales are $#{avg_sales}"
+
+# ans = Store.count(Store.where("stores.annual_revenue >= 1000000"))
+ans = Store.where("stores.annual_revenue >= 1000000").count
+puts "Number of stores with annual sales over $1M --> #{ans}"
