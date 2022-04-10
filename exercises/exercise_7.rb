2d851f1a7e8c7cf66dca05e963dbe2186400c3c9
lenilsamuel <lenilvarghese@gmail.com>
2022-04-09 19:03:19 -0600

    Finish validations and complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please input a store name"
+store_name = $stdin.gets.chomp.to_s
+puts "Attemptting to create a store with #{store_name}"
+new_store = Store.create(name: store_name)
+puts new_store.errors.full_messages
